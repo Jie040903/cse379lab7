dd4aa768c94f66a1e722c0df3cc2ee4726f94d00
Jie040903 <132932667+Jie040903@users.noreply.github.com>
2025-04-22 18:05:40 -0400

    Add files via upload

--- lab7/lab_7.s
+++ lab7/lab_7.s
@@ -33,6 +33,9 @@
 	.global ball_up_down_speed
 	.global ball_R_L
 	.global ball_R_L_speed
+	.global play1_head
+	.global play2_head
+	.global num_place_hold
 
 
 prompt: .string "Your prompt with instructions is place here", 0
@@ -47,18 +50,19 @@ game_board_black: .string 27, "[40m ", 0
 game_board_blue: .string 27, "[44m ", 0
 game_board_red: .string 27, "[41m ", 0
 game_board_move: .string 27, "[", 0					; after game_board_move, user output character for the amount of space want to move
-													; output character "A" for up
-													; output character "B" for down
-													; output character "C" for right
-													; output character "D" for down
+													; output character "A" for up		0x41
+													; output character "B" for down		0x42
+													; output character "C" for right	0x43
+													; output character "D" for left		0x44
+													; output character "s" to save		0x73
+													; output character "u" to save		0x75
 ball_start_place: .string 27, "[15;40H", 0
 player1_score_place: .string 27, "[2;2H", 0
 player2_score_place: .string 27, "[2;74H", 0
 timer_place: .string 27, "[2;32H", 0
 timer_text: .string "Time: ", 0
 timer_number: .string "0", 0
-player1_number: .string "0", 0
-player2_number: .string "0", 0
+num_place_hold: .string "a placeholder for int to string", 0
 
 
 
@@ -71,12 +75,16 @@ cursor_Line: .byte 0x01				; the line cursor is at
 cursor_Column: .byte 0x01			; the Column cursor is at
 play1_p_size: .byte 0x04			; the size of player1 penalties size
 play2_p_size: .byte 0x04			; the size of player2 penalties size
+play1_head: .byte 0x0E				; start at 15
+play2_head: .byte 0x0E				; start at 15
+player1_number: .byte 0x00
+player2_number: .byte 0x00
 ball_up_down: .byte 0x00			; if 0x00, ball moves up
 									; 0x01, ball moves down
-ball_up_down_speed: .byte 0x01		; the amount of space the ball move up or down
+ball_up_down_speed: .byte 0x00		; the amount of space the ball move up or down
 ball_R_L: .byte 0x00				; if 0x00, ball moves right
 									; 0x01, ball moves left
-ball_R_L_speed: .byte 0x00		; the amount of space the ball move right or left
+ball_R_L_speed: .byte 0x01		; the amount of space the ball move right or left
 
 
 	.text
@@ -133,6 +141,9 @@ ptr_to_ball_up_down: .word ball_up_down
 ptr_to_ball_up_down_speed: .word ball_up_down_speed
 ptr_to_ball_R_L: .word ball_R_L
 ptr_to_ball_R_L_speed: .word ball_R_L_speed
+ptr_to_play1_head: .word play1_head
+ptr_to_play2_head: .word play2_head
+ptr_to_num_place_hold: .word num_place_hold
 
 
 lab7:				; This is your main routine which is called from
@@ -140,6 +151,7 @@ lab7:				; This is your main routine which is called from
 	PUSH {r4-r12,lr}   	; Preserve registers to adhere to the AAPCS
 
  	bl uart_init
+ 	bl uart_interrupt_init
 
 	LDR r0, ptr_to_game_board_Gray
 	BL output_string
@@ -176,7 +188,7 @@ white_loop:
  	ADD r5, r5, #1
 	STRB r5, [r4]
 
-	CMP r5, #81
+	CMP r5, #82
 	BEQ white_done
 
 
@@ -219,7 +231,7 @@ black_loop:
 	LDR r0, ptr_to_cursor_Line
  	LDRB r4, [r0]
 
-	CMP r4, #79
+	CMP r4, #80
 	BEQ black_down_loop
 
 	B black_loop
@@ -229,7 +241,7 @@ black_down_loop:
 	LDR r0, ptr_to_cursor_Column
  	LDRB r2, [r0]
 
-	CMP r2, #26
+	CMP r2, #24
 	BEQ White2
 
 	B white_done
@@ -263,7 +275,7 @@ White2_part2:
  	ADD r5, r5, #1
 	STRB r5, [r4]
 
-	CMP r5, #81
+	CMP r5, #82
 	BEQ playerP
 
 	LDR r0, ptr_to_game_board_White
@@ -280,7 +292,7 @@ playerP:
 
 	MOV r0, #0x31
 	Bl output_character
-	MOV r0, #0x34
+	MOV r0, #0x33
 	Bl output_character
 	MOV r0, #0x41
 	Bl output_character
@@ -298,7 +310,7 @@ playerer_p_print:
 	BL output_string
 	MOV r0, #0x37
 	Bl output_character
-	MOV r0, #0x37
+	MOV r0, #0x38
 	Bl output_character
 	MOV r0, #0x43
 	Bl output_character
@@ -339,13 +351,19 @@ draw_ball:
 	LDR r0, ptr_to_text_White
 	BL output_string
 
-	LDR r0, ptr_to_player1_number
+	LDR r0, ptr_to_num_place_hold
+	LDR r1, ptr_to_player1_number
+	LDRB r1, [r1]
+	BL int2string
 	BL output_string
 
 	LDR r0, ptr_to_player2_score_place
 	BL output_string
 
-	LDR r0, ptr_to_player2_number
+	LDR r0, ptr_to_num_place_hold
+	LDR r1, ptr_to_player1_number
+	LDRB r1, [r1]
+	BL int2string
 	BL output_string
 
 	LDR r0, ptr_to_timer_place
@@ -572,16 +590,16 @@ UART0_Handler:
 
 	BL simple_read_character
 
-	CMP r0, #0x77		; see if enter is "w"
+	CMP r0, #0x77		; see if enter is "w" player 1 move up
 	BEQ is_w
 
-	CMP r0, #0x61		; see if enter is "a"
+	CMP r0, #0x61		; see if enter is "a" player 2 move up
 	BEQ is_a
 
-	CMP r0, #0x73		; see if enter is "s"
+	CMP r0, #0x73		; see if enter is "s" player 1 move down
 	BEQ is_s
 
-	CMP r0, #0x64		; see if enter is "d"
+	CMP r0, #0x64		; see if enter is "d" player 2 move down
 	BEQ is_d
 
 	; if input is not wasd, do nothing
@@ -589,34 +607,453 @@ UART0_Handler:
 	BX lr
 
 is_w:
-	LDR r4, ptr_to_mydata	; store  w  in mydata
-	MOV r5, #0x77
-	STRB r5, [r4]
+	;see if plater 1 can move P	
+	LDR r0, ptr_to_ball_R_L
+	LDRB r1, [r0]
+	CMP r1, #0	;if r0 is 0 this means ball is moving to player2
+	BNE w_part2
+	LDR r0, ptr_to_cursor_Line	; if ball is moving aways from player 1's P and the ball is 3 space away from the P
+	LDRB r0, [r0]
+	CMP r0, #3
+	BGT player1_loss_by_move_P
+	
+w_part2:	
+	LDR r3, ptr_to_play1_head
+	LDRB r4, [r3]
+	CMP r4, #4
+	BEQ P_move_end
+
+	; update play1_head to the new head
+	SUB r4, r4, #1
+	STRB r4, [r3]
+
+	; save the Cursor Position of the ball now
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+	MOV r0, #0x73
+	BL output_character
+
+	; draw the new P
+	; move to the head of player1 P
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+
+	LDR r0, ptr_to_num_place_hold
+	LDR r1, ptr_to_play1_head
+	LDRB r1, [r1]
+	BL int2string
+	BL output_string
+
+	MOV r0, #0x3B			; 0x3b = ;
+	BL output_character
+	MOV r0, #0x31			; 0x31 = 1
+	BL output_character
+	MOV r0, #0x48			; 0x48 = H
+	BL output_character
+
+	; draw the new head
+	LDR r0, ptr_to_game_board_White
+	BL output_string
+
+	MOV r0, #0x0D
+ 	BL output_character
+
+	; move to the tail of the P and make it black
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+	LDR r1, ptr_to_play1_p_size
+	LDRB r1, [r1]
+	LDR r0, ptr_to_num_place_hold
+	BL int2string
+	BL output_string
+	MOV r0, #0x42
+	BL output_character
+
+	LDR r0, ptr_to_game_board_black
+	BL output_string
+
+	
+	; move the CP back to where the ball is
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+
+	MOV r0, #0x75
+	BL output_character
 
 	POP {r4-r12,lr}  	; Restore registers from stack
 	BX lr
 
 is_a:
-	LDR r4, ptr_to_mydata	; store  a  in mydata
-	MOV r5, #0x61
-	STRB r5, [r4]
+	;see if plater 2 can move P	
+	LDR r0, ptr_to_ball_R_L
+	LDRB r0, [r0]
+	CMP r0, #1	;if r0 is 1 this means ball is moving to player1
+	BNE a_part2
+	LDR r0, ptr_to_cursor_Line	; if ball is moving aways from player 2's P and the ball is 3 space away from the P
+	LDRB r0, [r0]
+	CMP r0, #77
+	BLT player2_loss_by_move_P
+
+a_part2:
+	LDR r3, ptr_to_play2_head
+	LDRB r4, [r3]
+	CMP r4, #4
+	BEQ P_move_end
+
+	; update play2_head to the new head
+	SUB r4, r4, #1
+	STRB r4, [r3]
+
+	; save the Cursor Position of the ball now
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+	MOV r0, #0x73
+	BL output_character
+
+	; draw the new P
+	; move to the head of player2 P
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+
+	LDR r0, ptr_to_num_place_hold
+	LDR r1, ptr_to_play2_head
+	LDRB r1, [r1]
+	BL int2string
+	BL output_string
+
+	MOV r0, #0x3B			; 0x3b = ;
+	BL output_character
+	MOV r0, #0x38			; 0x38 = 8
+	BL output_character
+	MOV r0, #0x30			; 0x30 = 0
+	BL output_character
+	MOV r0, #0x48			; 0x48 = H
+	BL output_character
+
+	; draw the new head
+	LDR r0, ptr_to_game_board_White
+	BL output_string
+
+	; Move one space left
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+	MOV r0, #0x31
+	BL output_character
+	MOV r0, #0x44
+	BL output_character
+
+	; move to the tail of the P and make it black
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+	LDR r1, ptr_to_play2_p_size
+	LDRB r1, [r1]
+	LDR r0, ptr_to_num_place_hold
+	BL int2string
+	BL output_string
+	MOV r0, #0x42
+	BL output_character
+
+	LDR r0, ptr_to_game_board_black
+	BL output_string
+
+	
+	; move the CP back to where the ball is
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+
+	MOV r0, #0x75
+	BL output_character
 
 	POP {r4-r12,lr}  	; Restore registers from stack
 	BX lr
 
 is_s:
-	LDR r4, ptr_to_mydata	; store  s  in mydata
-	MOV r5, #0x73
+	;see if plater 1 can move P	
+	LDR r0, ptr_to_ball_R_L
+	LDRB r0, [r0]
+	CMP r0, #0	;if r0 is 0 this means ball is moving to player2
+	BNE s_part2
+	LDR r0, ptr_to_cursor_Line	; if ball is moving aways from player 1's P and the ball is 3 space away from the P
+	LDRB r0, [r0]
+	CMP r0, #3
+	BGT player1_loss_by_move_P
+
+s_part2:
+	LDR r4, ptr_to_play1_head
+	LDRB r4, [r4]
+	CMP r4, #23
+	BEQ P_move_end
+
+	; save the Cursor Position of the ball now
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+	MOV r0, #0x73
+	BL output_character
+
+	; draw the new P
+	; move to the head of player1 P
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+
+	LDR r0, ptr_to_num_place_hold
+	LDR r1, ptr_to_play1_head
+	LDRB r1, [r1]
+	BL int2string
+	BL output_string
+
+	MOV r0, #0x3B			; 0x3b = ;
+	BL output_character
+	MOV r0, #0x31			; 0x38 = 1
+	BL output_character
+	MOV r0, #0x48			; 0x48 = H
+	BL output_character
+	
+	;make the head black
+	LDR r0, ptr_to_game_board_black
+	BL output_string
+
+	; move to the tail of the P and make it new P
+	; Move one space left
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+	MOV r0, #0x31
+	BL output_character
+	MOV r0, #0x44
+	BL output_character
+
+	; move down play1_p_size
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+	LDR r1, ptr_to_play1_p_size
+	LDRB r1, [r1]
+	LDR r0, ptr_to_num_place_hold
+	BL int2string
+	BL output_string
+	MOV r0, #0x42
+	BL output_character
+
+	LDR r0, ptr_to_game_board_White
+	BL output_string
+
+	; updated the new head to play1_head
+	LDR r4, ptr_to_play1_head
+	LDRB r5, [r4]
+	ADD r5, r5, #1
 	STRB r5, [r4]
+
+	; move the CP back to where the ball is
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+
+	MOV r0, #0x75
+	BL output_character
 
 	POP {r4-r12,lr}  	; Restore registers from stack
 	BX lr
 
 is_d:
-	LDR r4, ptr_to_mydata	; store  d  in mydata
-	MOV r5, #0x64
+	;see if plater 2 can move P	
+	LDR r0, ptr_to_ball_R_L
+	LDRB r0, [r0]
+	CMP r0, #1	;if r0 is 1 this means ball is moving to player1
+	BNE d_part2
+	LDR r0, ptr_to_cursor_Line	; if ball is moving aways from player 2's P and the ball is 3 space away from the P
+	LDRB r0, [r0]
+	CMP r0, #77
+	BLT player2_loss_by_move_P
+
+d_part2:
+	LDR r4, ptr_to_play2_head
+	LDRB r4, [r4]
+	CMP r4, #23
+	BEQ P_move_end
+
+	; save the Cursor Position of the ball now
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+	MOV r0, #0x73
+	BL output_character
+
+	; draw the new P
+	; move to the head of player2 P
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+
+	LDR r0, ptr_to_num_place_hold
+	LDR r1, ptr_to_play2_head
+	LDRB r1, [r1]
+	BL int2string
+	BL output_string
+
+	MOV r0, #0x3B			; 0x3b = ;
+	BL output_character
+	MOV r0, #0x38			; 0x38 = 8
+	BL output_character
+	MOV r0, #0x30			; 0x30 = 0
+	BL output_character
+	MOV r0, #0x48			; 0x48 = H
+	BL output_character
+	
+	;make the head black
+	LDR r0, ptr_to_game_board_black
+	BL output_string
+
+	; move to the tail of the P and make it new P
+	; Move one space left
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+	MOV r0, #0x31
+	BL output_character
+	MOV r0, #0x44
+	BL output_character
+	
+	; move down play2_p_size
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+	LDR r1, ptr_to_play2_p_size
+	LDRB r1, [r1]
+	LDR r0, ptr_to_num_place_hold
+	BL int2string
+	BL output_string
+	MOV r0, #0x42
+	BL output_character
+
+	LDR r0, ptr_to_game_board_White
+	BL output_string
+
+	; updated the new head to play2_head
+	LDR r4, ptr_to_play2_head
+	LDRB r5, [r4]
+	ADD r5, r5, #1
 	STRB r5, [r4]
 
+	; move the CP back to where the ball is
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+
+	MOV r0, #0x75
+	BL output_character
+
+P_move_end:
+	POP {r4-r12,lr}  	; Restore registers from stack
+	BX lr
+
+player1_loss_by_move_P:
+	; Move one space left
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+	MOV r0, #0x31
+	BL output_character
+	MOV r0, #0x44
+	BL output_character
+	LDR r0, ptr_to_game_board_black		; clear ball on board
+	BL output_string
+	
+	LDR r0, ptr_to_player2_score_place	; move to the place of player1's score
+	Bl output_string
+
+	LDR r0, ptr_to_game_board_Gray
+	BL output_string
+
+	LDR r0, ptr_to_text_White
+	BL output_string
+	
+	LDR r0, ptr_to_player2_number			; updated p1's score and print
+	LDRB r1, [r0]
+	ADD r1, r1, #1
+	STRB r1, [r0]
+	LDR r0, ptr_to_num_place_hold
+	BL int2string
+	BL output_string
+
+	; draw ball at the starting place
+	LDR r0, ptr_to_ball_start_place
+	BL output_string
+
+	LDR r0, ptr_to_game_board_red
+	BL output_string
+
+	LDR r0, ptr_to_cursor_Column
+ 	MOV r2, #14
+ 	STRB r2, [r0]
+
+	LDR r4, ptr_to_cursor_Line
+ 	LDRB r5, [r4]
+ 	MOV r5, #40
+	STRB r5, [r4]
+	
+	; ball will move right and 0 up down
+	LDR r0, ptr_to_ball_up_down_speed
+	MOV r2, #0
+	LDRB r2, [r0]
+
+	LDR r0, ptr_to_ball_R_L	
+	MOV r1, #0
+	STRB r1, [r0]
+
+	LDR r0, ptr_to_ball_R_L_speed
+	MOV r2, #1
+	LDRB r2, [r0]
+	
+	
+	POP {r4-r12,lr}  	; Restore registers from stack
+	BX lr
+player2_loss_by_move_P:
+	; Move one space left
+	LDR r0, ptr_to_game_board_move
+	BL output_string
+	MOV r0, #0x31
+	BL output_character
+	MOV r0, #0x44
+	BL output_character
+	LDR r0, ptr_to_game_board_black		; clear ball on board
+	BL output_string
+	
+	LDR r0, ptr_to_player1_score_place	; move to the place of player2's score
+	Bl output_string
+
+	LDR r0, ptr_to_game_board_Gray
+	BL output_string
+
+	LDR r0, ptr_to_text_White
+	BL output_string
+	
+	LDR r0, ptr_to_player1_number			; updated p2's score and print
+	LDRB r1, [r0]
+	ADD r1, r1, #1
+	STRB r1, [r0]
+	LDR r0, ptr_to_num_place_hold
+	BL int2string
+	BL output_string
+
+	; draw ball at the starting place
+	LDR r0, ptr_to_ball_start_place
+	BL output_string
+
+	LDR r0, ptr_to_game_board_red
+	BL output_string
+
+	LDR r0, ptr_to_cursor_Column
+ 	MOV r2, #14
+ 	STRB r2, [r0]
+
+	LDR r4, ptr_to_cursor_Line
+ 	LDRB r5, [r4]
+ 	MOV r5, #40
+	STRB r5, [r4]
+	
+	; ball will move left and 0 up down
+	LDR r0, ptr_to_ball_up_down_speed
+	MOV r2, #0
+	LDRB r2, [r0]
+
+	LDR r0, ptr_to_ball_R_L	
+	MOV r1, #1
+	STRB r1, [r0]
+
+	LDR r0, ptr_to_ball_R_L_speed
+	MOV r2, #1
+	LDRB r2, [r0]
 	POP {r4-r12,lr}  	; Restore registers from stack
 	BX lr
 
@@ -658,8 +1095,7 @@ Timer_Handler:
 	LDR r0, ptr_to_game_board_move
 	BL output_string
 
-	ADD r2, r2, #0x30
-	MOV r0, r2
+	ADD r0, r2, #0x30
 	Bl output_character
 
 	MOV r0, #0x41
@@ -707,15 +1143,179 @@ ball_down:
 
 see_ball_move_RL:
 	LDR r0, ptr_to_ball_R_L
-	LDRB r0, [r0]
 	LDRB r1, [r0]
 
 	LDR r0, ptr_to_ball_R_L_speed
-	LDRB r0, [r0]
 	LDRB r2, [r0]
 
 	CMP r2, #0
 	BEQ ball_moving_done
+
+	; see if the ball will move to P's head
+	LDR r0, ptr_to_cursor_Line
+	LDRB r3, [r0]	
+	CMP r3, #2
+	BEQ ball_at_p1
+	CMP r3, #79
+	BEQ ball_at_p2
+
+	B see_ball_move_RL_part2
+
+ball_at_p1:
+	LDR r0, ptr_to_cursor_Column
+	LDRB r3, [r0]
+	LDR r0, ptr_to_play1_head
+	LDRB r4, [r0]
+	CMP r3, r4			; ball at p's head, will move 1 up and left
+	BEQ ball_at_p1_head
+	
+	CMP r3, r4			; Ball move above P's heand p1 loss
+	BLT player1_loss_by_move_P
+
+	LDR r0, ptr_to_play1_p_size
+	LDRB r5, [r0]
+	ADD r5, r5, r4
+	SUB r5, r5, #1
+	CMP r3, r5
+	BEQ ball_at_p1_tail		; ball at p's tail, will move 1 down and left
+	
+	CMP r3, r5			; Ball move below P's heand p1 loss
+	BGT player1_loss_by_move_P
+
+	; at this point, the ball will hit the middle of P, so it just move right
+	LDR r0, ptr_to_ball_R_L	
+	MOV r1, #1
+	STRB r1, [r0]
+	
+	LDR r0, ptr_to_ball_R_L_speed
+	MOV r1, #1
+	STRB r1, [r0]
+
+	LDR r0, ptr_to_ball_up_down
+	MOV r1, #0
+	STRB r1, [r0]
+
+	LDR r0, ptr_to_ball_up_down_speed
+	MOV r1, #0
+	STRB r1, [r0]
+	B see_ball_move_RL_part2
+
+ball_at_p1_head:
+	LDR r0, ptr_to_ball_R_L	
+	MOV r1, #1
+	STRB r1, [r0]
+	
+	LDR r0, ptr_to_ball_R_L_speed
+	MOV r1, #1
+	STRB r1, [r0]
+
+	LDR r0, ptr_to_ball_up_down
+	MOV r1, #0
+	STRB r1, [r0]
+
+	LDR r0, ptr_to_ball_up_down_speed
+	MOV r1, #1
+	STRB r1, [r0]
+	B see_ball_move_RL_part2
+
+ball_at_p1_tail:
+	LDR r0, ptr_to_ball_R_L
+	MOV r1, #1
+	STRB r1, [r0]
+	
+	LDR r0, ptr_to_ball_R_L_speed
+	MOV r1, #1
+	STRB r1, [r0]
+
+	LDR r0, ptr_to_ball_up_down
+	MOV r1, #1
+	STRB r1, [r0]
+
+	LDR r0, ptr_to_ball_up_down_speed
+	MOV r1, #1
+	STRB r1, [r0]
+	B see_ball_move_RL_part2
+
+ball_at_p2:
+	LDR r0, ptr_to_cursor_Column
+	LDRB r3, [r0]
+	LDR r0, ptr_to_play2_head
+	LDRB r4, [r0]
+	CMP r3, r4			; ball at p's head, will move 1 up and left
+	BEQ ball_at_p2_head
+	
+	CMP r3, r4			; Ball move above P's heand p2 loss
+	BLT player2_loss_by_move_P
+
+	LDR r0, ptr_to_play2_p_size
+	LDRB r5, [r0]
+	ADD r5, r5, r4
+	SUB r5, r5, #1
+	CMP r3, r5
+	BEQ ball_at_p2_tail		; ball at p's tail, will move 1 down and left
+	
+	CMP r3, r5			; Ball move below P's heand p2 loss
+	BGT player2_loss_by_move_P
+
+	; at this point, the ball will hit the middle of P, so it just move left
+	LDR r0, ptr_to_ball_R_L	
+	MOV r1, #0
+	STRB r1, [r0]
+	
+	LDR r0, ptr_to_ball_R_L_speed
+	MOV r1, #1
+	STRB r1, [r0]
+
+	LDR r0, ptr_to_ball_up_down
+	MOV r1, #0
+	STRB r1, [r0]
+
+	LDR r0, ptr_to_ball_up_down_speed
+	MOV r1, #0
+	STRB r1, [r0]
+	B see_ball_move_RL_part2
+
+ball_at_p2_head:
+	LDR r0, ptr_to_ball_R_L	
+	MOV r1, #0
+	STRB r1, [r0]
+	
+	LDR r0, ptr_to_ball_R_L_speed
+	MOV r1, #1
+	STRB r1, [r0]
+
+	LDR r0, ptr_to_ball_up_down
+	MOV r1, #0
+	STRB r1, [r0]
+
+	LDR r0, ptr_to_ball_up_down_speed
+	MOV r1, #1
+	STRB r1, [r0]
+	B see_ball_move_RL_part2
+
+ball_at_p2_tail:
+	LDR r0, ptr_to_ball_R_L
+	MOV r1, #0
+	STRB r1, [r0]
+	
+	LDR r0, ptr_to_ball_R_L_speed
+	MOV r1, #1
+	STRB r1, [r0]
+
+	LDR r0, ptr_to_ball_up_down
+	MOV r1, #1
+	STRB r1, [r0]
+
+	LDR r0, ptr_to_ball_up_down_speed
+	MOV r1, #1
+	STRB r1, [r0]
+	
+see_ball_move_RL_part2:
+	LDR r0, ptr_to_ball_R_L
+	LDRB r1, [r0]
+
+	LDR r0, ptr_to_ball_R_L_speed
+	LDRB r2, [r0]
 
 	CMP r1, #0x00
 	BNE ball_left
